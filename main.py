7a23f23db67e1cd93082a4b00744a3b8e1dc24b9
Evan J <42357644+ejach@users.noreply.github.com>
2023-02-19 13:37:03 -0500

    #8 - fixed infinite loop

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from tarfile import open
 from time import sleep
 
 from mega import Mega
-from mega.errors import RequestError
 from schedule import every, run_pending
 
-# Setup logger
+
+# Setup logger and global variables
 basicConfig(stream=stdout, level=INFO)
+
+now = datetime.now()
+DATE_TIME = now.strftime('%m-%d-%Y_%H:%M')
+FILE_NAME = f'backup-{DATE_TIME}.tar.gz'
 
 
 # Create tar.gz file in target directory
@@ -23,43 +27,35 @@ def ball_dir(targ_dir, filename):
                 tar_handle.add(path.join(root, f))
 
 
-# Upload the tarball at specified time
-def upload(mega_obj, target_dir):
-    now = datetime.now()
-    date_time = now.strftime('%m-%d-%Y_%H:%M')
+# Compress the files, return if the file can be uploaded
+def can_upload():
+    info(f'******* Log info for {DATE_TIME} *******')
 
-    info(f'******* Log info for {date_time} *******')
+    # Compress the files in the target directory
+    ball_dir(target_dir, FILE_NAME)
 
-    file_name = f'backup-{date_time}.tar.gz'
-    # Compress the file in the target directory
-    ball_dir(target_dir, file_name)
-    file_size = path.getsize('%s/%s' % (getcwd(), file_name))
+    # Calculate if the tarball can be uploaded
+    file_size = path.getsize('%s/%s' % (getcwd(), FILE_NAME))
     storage_space = m.get_storage_space()
     used_space = storage_space['used']
     total_space = storage_space['total'] - used_space
+    return file_size < total_space
 
+# Upload the tarball at specified time
+def upload(m, file_name):
     info('Starting upload to mega.nz')
+    m.upload(file_name)
 
-    try:
-        if file_size < total_space:
-            mega_obj.upload(file_name)
-        else:
-            # Throw QuotaError, catch it and wait until the next day and try again
-            raise RequestError(-17)
-    except RequestError:
-        error('File cannot be uploaded, quota needs to be freed up.')
-
-    file_found = mega_obj.find(file_name, exclude_deleted=True)
+    file_found = m.find(file_name, exclude_deleted=True)
 
     if file_found:
         if path.exists(file_name):
-            info(f'Upload completed for {date_time}. Deleting file')
+            info(f'Upload completed for {DATE_TIME}. Deleting file')
             remove(file_name)
         else:
             error('File not found.')
     else:
         error('File not uploaded, please try again.')
-
 
 # Create the Mega object and log in using env variables
 info('Logging in')
@@ -74,9 +70,14 @@ chdir('..')
 info('Current working directory is ' + getcwd())
 
 # Run upload at the specified BACKUP_TIME
-every().day.at(getenv('BACKUP_TIME')).do(upload(m, target_dir))
+every().day.at(getenv('BACKUP_TIME')).do(upload(m, FILE_NAME))
 
-# Run the pending tasks every 1s
+# Run the pending tasks every 1s, making sure that there is enough space
 while True:
-    run_pending()
-    sleep(1)
+    if can_upload():
+        run_pending()
+        sleep(1)
+    else:
+        # Wait an hour, then check if there is enough space
+        sleep(3600)
+        continue
